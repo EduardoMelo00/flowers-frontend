5902d219c32cb28488a764096ae67e5bf0e0cb80
EduardoMeloDev <78520501+EduardoMeloDev@users.noreply.github.com>
2023-06-21 16:32:38 -0300

    adjust videos

--- src/components/videoPage/VideoPlayerPage.js
+++ src/components/videoPage/VideoPlayerPage.js
@@ -7,10 +7,11 @@ const VideoPlayerPage = () => {
 
   return (
     <div className="video-container">
-     <video width="1024" height="680" controls controlsList="nodownload" onContextMenu={(e) => e.preventDefault()} autoPlay playsInline>
+    <video width="1024" height="680" controls controlsList="nodownload" onContextMenu={(e) => e.preventDefault()} autoPlay muted onError={(e) => console.error('Video error:', e)}>
     <source src={decodedVideoUrl} type="video/mp4"/>
     Your browser does not support the video tag.
 </video>
+
     </div>
   );
 }
