99bfe09c5e244b1974d0ecb3478c5004f5aa28ea
EduardoMeloDev <78520501+EduardoMeloDev@users.noreply.github.com>
2023-06-22 18:25:34 -0300

    adjust forget password

--- src/components/liveVideoPage/liveVideoPage.js
+++ src/components/liveVideoPage/liveVideoPage.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import YouTube from 'react-youtube';
 import './liveVideoPage.css';
-
-
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const LiveVideoPage = () => {
     const [countdown, setCountdown] = useState(7200);  // 2 hours in seconds
     const [showVideo, setShowVideo] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);  // New state for user authentication status
 
     useEffect(() => {
         const timerId = setInterval(() => {
@@ -20,6 +21,25 @@ const LiveVideoPage = () => {
                 }
             });
         }, 1000);  // countdown every 1000ms (or 1s)
+
+        // New effect for checking user authentication
+        const checkUserAuthentication = async () => {
+            try {
+                const token = Cookies.get('token'); // get token from cookies
+                const response = await axios.get('https://flowers-node-backend-2c4af429ac26.herokuapp.com/api/auth/checkToken', {
+                    headers: {
+                        'Authorization': `Bearer ${token}` // Send token in Authorization header
+                    }
+                });
+                setIsAuthenticated(response.status === 200);
+            } catch (error) {
+                console.error(error);
+                setIsAuthenticated(false);
+            }
+        };
+
+        checkUserAuthentication();
+
         return () => clearInterval(timerId);  // clean up the interval on unmount
     }, []);
 
@@ -45,12 +65,16 @@ const LiveVideoPage = () => {
 
     return (
         <div className="video-container">
-            {showVideo ? (
-                <YouTube videoId={videoId} opts={opts} className="video"/>
+            {isAuthenticated ? (  // Check user authentication before showing video or countdown
+                showVideo ? (
+                    <YouTube videoId={videoId} opts={opts} className="video"/>
+                ) : (
+                    <h1 className="countdown" style={{ color: 'white' }}>
+                        O culto iniciará em : {hours}:{minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+                    </h1>
+                )
             ) : (
-                <h1 className="countdown" style={{ color: 'white' }}>
-                    O culto iniciará em : {hours}:{minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}
-                </h1>
+                <h1>You need to login to view this content.</h1>
             )}
         </div>
     );
