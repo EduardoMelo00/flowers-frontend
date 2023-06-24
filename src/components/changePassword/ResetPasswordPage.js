8f71f5066c2c5fc4c9f153e9109b655373ebf1c9
EduardoMeloDev <78520501+EduardoMeloDev@users.noreply.github.com>
2023-06-24 04:44:28 -0300

    adjust forget password

--- src/components/changePassword/ResetPasswordPage.js
+++ src/components/changePassword/ResetPasswordPage.js
@@ -6,6 +6,9 @@ function ResetPasswordPage() {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [message, setMessage] = useState('');
+  const [email, setEmail] = useState('');
+
+
 
   const search = useLocation().search;
   const token = new URLSearchParams(search).get('token');
@@ -21,10 +24,10 @@ function ResetPasswordPage() {
     }
 
     try {
-      const response = await axios.post('https://flowers-node-backend-2c4af429ac26.herokuapp.com/api/auth/reset-password', { password, token });
+      const response = await axios.post('https://flowers-node-backend-2c4af429ac26.herokuapp.com/api/auth/reset-password', { password, email });
 
       if (response.status === 200) {
-        history.push('/success');
+        history('/success');
       }
     } catch (error) {
       if (error.response) {
@@ -52,6 +55,19 @@ function ResetPasswordPage() {
       <h2>Resetar senha</h2>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
+
+        <label style={inputStyle} htmlFor="email">
+            Email
+            <input
+              style={inputStyle}
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+
           <label style={inputStyle} htmlFor="password">
             Nova senha
             <input
