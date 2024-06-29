210eeca43aeefa681d00a26ca87be7ee88621dfa
EduardoMeloDev <78520501+EduardoMeloDev@users.noreply.github.com>
2024-06-29 17:20:34 -0300

    culto sabado

--- src/components/liveVideoPage/liveVideoPage.js
+++ src/components/liveVideoPage/liveVideoPage.js
@@ -36,9 +36,8 @@ const LiveVideoPage = () => {
             showinfo: 0
         },
     };
-    const videoId = "a8TI7up4IFQ"; 
- 
-
+    const videoId = "w_4CM7LAqxo"; 
+   
     const hours = Math.floor(countdown / 3600);
     const minutes = Math.floor((countdown - (hours * 3600)) / 60);
     const seconds = countdown - (hours * 3600) - (minutes * 60);
