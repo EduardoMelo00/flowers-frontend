04d38bf41232b119998e5b1e65b503fe30d9ef96
EduardoMeloDev <78520501+EduardoMeloDev@users.noreply.github.com>
2024-06-29 11:33:31 -0300

    video sabado a tarde

--- src/components/liveVideoPage/liveVideoPage.js
+++ src/components/liveVideoPage/liveVideoPage.js
@@ -36,9 +36,9 @@ const LiveVideoPage = () => {
             showinfo: 0
         },
     };
-    const videoId = "zQdewZt1SOc"; 
+    const videoId = "a8TI7up4IFQ"; 
  
-   
+
     const hours = Math.floor(countdown / 3600);
     const minutes = Math.floor((countdown - (hours * 3600)) / 60);
     const seconds = countdown - (hours * 3600) - (minutes * 60);
